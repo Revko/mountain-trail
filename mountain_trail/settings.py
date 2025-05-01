3bb4e654a12074e94725b7b7554261801d9245d8
Ivan Revko <revko7692@gmail.com>
2025-05-01 22:11:50 +0300

    delete LOGOUT_REDIRECT_URL

--- mountain_trail/settings.py
+++ mountain_trail/settings.py
@@ -106,8 +106,6 @@ AUTH_USER_MODEL = "carpathians.Participant"
 
 LOGIN_REDIRECT_URL = "/"
 
-LOGOUT_REDIRECT_URL = "/"
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
