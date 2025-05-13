53c5ebd40997dfa7fc8fbb862228b9322710bc4f
Ivan Revko <revko7692@gmail.com>
2025-05-13 21:07:23 +0300

    update settings

--- mountain_trail/settings.py
+++ mountain_trail/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
-DEBUG = config("DEBUG", default=False, cast=bool)
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-z$^&$3%vaz2i059vf1q@@r34mpk+54q+z@kku1^f%acljs4khp")
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
